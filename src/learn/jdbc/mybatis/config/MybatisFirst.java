dcaa678b34c9800d8f2d90a3a98ea774f4a7c410
manluqiusuo <lansekaola@163.com>
2017-12-05 09:56:24 +0800

    JDBC  了解

--- src/learn/jdbc/mybatis/config/MybatisFirst.java
+++ src/learn/jdbc/mybatis/config/MybatisFirst.java
@@ -1,4 +1,12 @@
-package learn.jdbc.mybatis.config;
+import learn.jdbc.mybatis.modal.Test;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * @description 
@@ -8,7 +16,7 @@ package learn.jdbc.mybatis.config;
 public class MybatisFirst {
     //根据id查询用户信息，得到一条记录结果
 
-    private void findUserByIdTest() throws IOException{
+    private void findUserByIdTest() throws IOException {
         // mybatis配置文件
         String resource = "SqlMapConfig.xml";
         // 得到配置文件流
@@ -24,37 +32,16 @@ public class MybatisFirst {
         // 第二个参数：指定和映射文件中所匹配的parameterType类型的参数
         // sqlSession.selectOne结果 是与映射文件中所匹配的resultType类型的对象
         // selectOne查询出一条记录
-        User user = sqlSession.selectOne("test.findUserById",'001');
+        Test user = sqlSession.selectOne("test.findUserById","001");
 
-        System.out.println(user);
+        System.out.println(user.toString());
 
         // 释放资源
         sqlSession.close();
 
     }
 
-    // 根据用户名称模糊查询用户列表
-
-    public void findUserByNameTest() throws IOException {
-        // mybatis配置文件
-        String resource = "SqlMapConfig.xml";
-        // 得到配置文件流
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-
-        // 创建会话工厂，传入mybatis的配置文件信息
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
-                .build(inputStream);
-
-        // 通过工厂得到SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        // list中的user和映射文件中resultType所指定的类型一致
-        List<User> list = sqlSession.selectList("test.findUserByName", "00");
-        System.out.println(list);
-        sqlSession.close();
-
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MybatisFirst mf = new MybatisFirst();
         mf.findUserByIdTest();
     }
